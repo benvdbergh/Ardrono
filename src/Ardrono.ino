41199c59d0b3dcc49c18fb0c73c4566a9cd78bc7
benvdbergh <benvdbergh97@gmail.com>
2023-01-12 01:12:11 +0100

    Add firmata comm

--- src/Ardrono.ino
+++ src/Ardrono.ino
@@ -1,19 +1,29 @@
 #include <Arduino.h>
+#include <Firmata.h>
+
 #include "Motion.h"
 #include "Motors.h"
 #include "Controller.h"
-#include "Filter.h"
 
 Motion motion;
 Controller controller;
 Motors motors;
-Filter filterX, filterY, filterZ, filterK;
+byte mode;
 
 float setpoint[3] = {0, 0, 0}; //[yaw pitch roll]
-float data[3] = {1.0, 0.0, 3.4};
+float db[12] = {};
+
+void analogReadCallback(byte tpc, int value){
+  db[tpc] = value;
+}
+
 
 void setup() {
-  Serial.begin(57600);
+  Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
+  Firmata.attach(ANALOG_MESSAGE, analogReadCallback);
+  Firmata.begin(57600);
+
+
   motors.Init();
   motion.Init();
   float pids[9];
@@ -21,11 +31,27 @@ void setup() {
 }
 
 void loop() {
+  // Read Host data
+  while (Firmata.available()) {
+    Firmata.processInput();
+  }
+
   // Get MPU sensor data
   motion.Main();
 
+  db[YAW] = motion.ypr[0];
+  db[PITCH] = motion.ypr[1];
+  db[ROLL] = motion.ypr[2];
+
   // Set setpoint based on mode
-  switch (1){
+  switch (mode){
+    case 0:     // Manual
+      motors.motorYpr[0] = db[SET_YAW];
+      motors.motorYpr[1] = db[SET_PITCH];
+      motors.motorYpr[2] = db[SET_ROLL];
+      motors.throttle = db[SET_THROTTLE];
+      break;
+
     case 1:     // Stabilize
       setpoint[0] = 0;
       setpoint[1] = 0;
@@ -37,6 +63,7 @@ void loop() {
       motors.motorYpr[0] = 0.0;
       motors.motorYpr[1] = 0.0;
       motors.motorYpr[2] = 0.0;
+      motors.throttle = 0.0;
       break;
   }
 
@@ -45,4 +72,18 @@ void loop() {
 
   // Write motorvalues to ESC's
   motors.Main();
+
+  // Receive and send new debug info from/to host
+  while (Firmata.available()) {
+    Firmata.processInput();
+  }
+
+  Serial.print(db[1]);
+  Serial.print("\t");
+  Serial.print(db[2]);
+  Serial.print("\t");
+  Serial.print(db[3]);
+  Serial.println("\t");
+
+  // Firmata.sendAnalog(yaw, motion.ypr[0])
 }
